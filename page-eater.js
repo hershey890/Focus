0b9cb7d9a26dbda916f4fa3f260a2b7a37c62c0c
Hersh Joshi <hersh.joshi1@gmail.com>
2020-11-23 15:06:50 -0800

    first commit

---
+++ page-eater.js
@@ -0,0 +1,7 @@
+document.body.textContent = "";
+
+var header = document.createElement('h1')
+header.textContent = "Stay on task. Get higher grades and more free time."
+document.body.appendChild(header);
+
+//webblockrequests in firefox may be good later
